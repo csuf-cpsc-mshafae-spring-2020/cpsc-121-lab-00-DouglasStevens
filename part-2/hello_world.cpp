0af1e231cfca3457acefcb1f1e58d99595b71ae9
amazingcheetos <amazingcheetos@cs104u-3e.acad.fullerton.edu>
2020-01-23 15:28:40 -0800

    Finished Part 2 of Lab 00

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,18 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+//Douglas Stevens CPSC 121-03 amazingcheetos@csu.fullerton.edu
+//hello_world.cpp
+//Say Hello to anything you want however many times you want.
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello world!" << endl;
+  if (argc <= 2){
+    cout << "Too few arguments.\n";
+    return 1;
+  }
+  int n = stoi(argv[2]);
+  for (int i = 0; i < n; i++){
+    cout << "Hello " << argv[1] << endl;
+  }
   return 0;
 }
